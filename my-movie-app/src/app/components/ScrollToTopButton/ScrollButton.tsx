bafcb619a52312bb42cd927562475ef5082b87e0
Yana Stoyanowa <y.stoyanova@blubito.com>
2024-11-02 12:11:18 +0200

    remove event listener after unmount

--- my-movie-app/src/app/components/ScrollToTopButton/ScrollButton.tsx
+++ my-movie-app/src/app/components/ScrollToTopButton/ScrollButton.tsx
@@ -1,5 +1,4 @@
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NorthIcon from "@mui/icons-material/North";
 import { Button } from "@mui/material";
 import { ScrollButtonStyles } from "./ScrollButtonStyles";
@@ -7,23 +6,29 @@ import { ScrollButtonStyles } from "./ScrollButtonStyles";
 const ScrollButton = (): JSX.Element => {
   const [visible, setVisible] = useState(false);
 
-  const toggleVisible = (): void => {
-    const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300) {
-      setVisible(true);
-    } else if (scrolled <= 300) {
-      setVisible(false);
-    }
-  };
+  useEffect(() => {
+    const toggleVisible = (): void => {
+      const scrolled = document.documentElement.scrollTop;
+      if (scrolled > 300) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
 
-  const scrollToTop = ():void => {
+    window.addEventListener("scroll", toggleVisible);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
+  const scrollToTop = (): void => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
-
-  window.addEventListener("scroll", toggleVisible);
 
   return (
     <Button
